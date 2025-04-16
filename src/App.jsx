c47b365ec386daa5609d797c09b005cccb23c4f4
SamyazaCode <lautaropaz__@hotmail.com>
2025-04-16 19:50:35 -0300

    Cambios en Navbar y limpieza de componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
 import Banner from './components/Banner'
-import Galeria from './components/Galeria'
 import Navbar from './components/Navbar'
-import Servicios from './components/Servicios'
 import SobreMi from './components/SobreMi'
-import Testimonios from './components/Testimonios'
-import Ubicacion from './components/Ubicacion'
 import Footer from './components/Footer'
-import Btncontacto from './components/Btncontacto'
 
 
 function App() {
@@ -15,12 +10,7 @@ function App() {
     <>
       <Navbar></Navbar>
       <Banner></Banner>
-      <Servicios></Servicios>
-      <Galeria></Galeria>
       <SobreMi></SobreMi>
-      <Testimonios></Testimonios>
-      <Ubicacion></Ubicacion>
-      <Btncontacto></Btncontacto>
       <Footer></Footer>
     </>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -38,16 +38,21 @@ const Navbar = () => {
   return (
     <>
     <nav className="navbar navbar-dark fixed-top">
-      <div className="container-fluid">
+      <div className="container-fluid d-flex justify-content-between align-items-center">
         
-        <a href="#inicio" onClick={scrollToTop}>
-          <img className='navbar-logo' src="imgs/logo-tatto.webp" width="60" height="60" fetchpriority="high" alt='logo' onClick={scrollToTop} />
-        </a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
-            <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
-          </svg>
-        </button>
+        {/* Columna izquierda vacía con mismo ancho que el botón */}
+        <div className="navbar-placeholder" />
+
+        <div className='navbar-contendor-elementos'>
+          <a href="#inicio" onClick={scrollToTop} className='navbar-center'>
+          <img className='navbar-logo' src="imgs/logo-lautaropaz.png" width="50" height="50" fetchpriority="high" alt='logo' onClick={scrollToTop} />
+          </a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
+              <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+            </svg>
+          </button>
+        </div>
 
         <div className="offcanvas offcanvas-end text-bg-dark" tabIndex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
           <div className="offcanvas-header">
@@ -66,53 +71,11 @@ const Navbar = () => {
               </li>
               <li className="nav-item">
                 <a 
-                  className={`nav-link ${activeSection === 'servicios' ? 'active' : ''}`} 
-                  href="#servicios"
-                >
-                  Servicios
-                </a>
-              </li>
-              <li className="nav-item">
-                <a 
-                  className={`nav-link ${activeSection === 'galeria' ? 'active' : ''}`} 
-                  href="#galeria"
-                >
-                  Galería
-                </a>
-              </li>
-              <li className="nav-item">
-                <a 
                   className={`nav-link ${activeSection === 'sobre-mi' ? 'active' : ''}`} 
                   href="#sobre-mi"
                   onClick={() => setActiveSection('sobre-mi')}
                 >
                   Sobre mí
-                </a>
-              </li>
-              <li className="nav-item">
-                <a 
-                  className={`nav-link ${activeSection === 'testimonios' ? 'active' : ''}`} 
-                  href="#testimonios"
-                >
-                  Testimonios
-                </a>
-              </li>
-              <li className="nav-item">
-                <a 
-                  className={`nav-link ${activeSection === 'ubicacion' ? 'active' : ''}`} 
-                  href="#ubicacion"
-                  onClick={() => setActiveSection('ubicacion')}
-                >
-                  Ubicacion
-                </a>
-              </li>
-              <li className="nav-item">
-                <a 
-                  className={`nav-link ${activeSection === 'contactarme' ? 'active' : ''}`} 
-                  href="#contactarme"
-                  onClick={() => setActiveSection('contactarme')}
-                >
-                  Contactarme
                 </a>
               </li>
             </ul>
