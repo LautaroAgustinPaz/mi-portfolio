f649b0702946f92c2caa7be4c228d3da297dbd81
SamyazaCode <lautaropaz__@hotmail.com>
2025-05-14 01:14:03 -0300

    Sobre Mi - Modificaciones

--- src/components/SobreMi.jsx
+++ src/components/SobreMi.jsx
@@ -35,25 +35,22 @@ const SobreMi = () => {
       </div>
 
         <div className="row sobre-mi_row">
-
             <div className="col-sm-12 col-md-6 col-lg-6 columna-biografia">
                 <h2 className='biografia_titulo'>Hola, soy</h2>
                 <h2 className='biografia-titulo-2'>Lautaro Paz</h2>
-                <p className='biografia_parrafo'>Me encanta crear y diseñar sitios web, que brinden una experiencia única como satisfactoria, poder intervenir en interfaces que tengan parte de mi creatividad y dedicación. <br /> <br /> 
+                <p className='biografia_parrafo'>Un Programador Full Stack de 23 años de Argentina. <br />
+                  
+                  Me gusta participar en proyectos donde el diseño y la performance van de la mano. Valoro el trabajo en equipo y busco siempre generar un ambiente empático y colaborativo. Estoy en constante aprendizaje, tanto a nivel técnico como personal.</p>
 
-                Tengo 23 años, soy un programador Full Stack, constantemente estoy aprendiendo nuevas tecnologias y herramientas, para mejorar mis habilidades. <br /> <br /> 
-
-                Espero que trabajemos juntos!</p>
+                <div className='grupo-redes-sociales'>
+                   <div className='contenedor-redondo'><a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faInstagram}  className='iconos-redes'/></a></div>
+                    <div className='contenedor-redondo'><a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faFacebook}  className='iconos-redes'/></a></div>
+                </div>
             </div>
                 <div className="col-sm-12 col-md-6 col-lg-6 columna-img">
                   <img className='foto-biografia' src="/imgs/foto-me.jpg" alt="foto-tatuador" loading="lazy"/>
                 </div>
         </div>
-                        {/* <div className='grupo-redes-sociales'>
-                    <div className='contenedor-redondo'><a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faInstagram}  className='iconos-redes'/></a></div>
-                    <div className='contenedor-redondo'><a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faFacebook}  className='iconos-redes'/></a></div>
-                    <div className='contenedor-redondo'><a href="https://www.tiktok.com/es/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faTiktok} className='iconos-redes'/></a></div>
-                </div> */}
     </section>
     
   )
