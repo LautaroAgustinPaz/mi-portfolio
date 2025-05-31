3452b4c9480152a684609f92d0d9808e48fdc9d1
SamyazaCode <lautaropaz__@hotmail.com>
2025-05-31 17:17:18 -0300

    Avanzamos con la sección Habilidades

--- src/components/Habilidades.jsx
+++ src/components/Habilidades.jsx
@@ -12,38 +12,30 @@ const Habilidades = () => {
       <div className="row contenedor-habilidades">
 
         <div className='card card-html col-lg-3'>
-          <img src="" alt="" srcset="" />
-          <h1>Hola</h1>
+          <img src="public/imgs/tecnologias/html.png" alt="html"/>
         </div>
         <div className='card card-html col-lg-3'>
-          <img src="" alt="" srcset="" />
-          <h1>Hola</h1>
+          <img src="public/imgs/tecnologias/css.png" alt="css"/>
         </div>
         <div className='card card-html col-lg-3'>
-          <img src="" alt="" srcset="" />
-          <h1>Hola</h1>
+          <img src="public/imgs/tecnologias/sass.png" alt="sass"/>
         </div>
         <div className='card card-html col-lg-3'>
-          <img src="" alt="" srcset="" />
-          <h1>Hola</h1>
+          <img src="public/imgs/tecnologias/bootstrap.png" alt=""/>
         </div>
         <div className='card card-html col-lg-3'>
-          <img src="" alt="" srcset="" />
-          <h1>Hola</h1>
+          <img src="public/imgs/tecnologias/js.png" alt=""/>
         </div>
         <div className='card card-html col-lg-3'>
-          <img src="" alt="" srcset="" />
-          <h1>Hola</h1>
+          <img src="public/imgs/tecnologias/react.png" alt=""/>
         </div>
         <div className='card card-html col-lg-3'>
-          <img src="" alt="" srcset="" />
-          <h1>Hola</h1>
+          <img src="public/imgs/tecnologias/nodejs.png" alt=""/>
         </div>
         <div className='card card-html col-lg-3'>
-          <img src="" alt="" srcset="" />
-          <h1>Hola</h1>
+          <img src="public/imgs/tecnologias/mongodb.png" alt=""/>
         </div>
-        
+    
       </div>
     </section>
   )
