6589a7703bc78b8432cb886dca918a4c8320b919
SamyazaCode <lautaropaz__@hotmail.com>
2025-06-08 21:34:13 -0300

    Ultimos detalles en sección Habilidades

--- src/components/Habilidades.jsx
+++ src/components/Habilidades.jsx
@@ -1,41 +1,74 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import ScrollReveal from 'scrollreveal';
+
+// Arrays de Imagenes tecnologias
+const tecnologiasUno = [
+  { src: '/imgs/tecnologias/html.png', alt: 'html' },
+  { src: '/imgs/tecnologias/css.png', alt: 'css' },
+  { src: '/imgs/tecnologias/sass.png', alt: 'sass' },
+  { src: '/imgs/tecnologias/bootstrap.png', alt: 'bootstrap' },
+];
+
+const tecnologiasDos = [
+  { src: '/imgs/tecnologias/js.png', alt: 'js' },
+  { src: '/imgs/tecnologias/react.png', alt: 'react' },
+  { src: '/imgs/tecnologias/nodejs.png', alt: 'nodejs' },
+  { src: '/imgs/tecnologias/mongodb.png', alt: 'mongodb' }
+];
 
 const Habilidades = () => {
+
+  // Para que se pause en mobile
+  useEffect(() => {
+    const filas = document.querySelectorAll('.fila');
+  
+    const pauseAnimation = () => filas.forEach(fila => fila.style.animationPlayState = 'paused');
+    const playAnimation = () => filas.forEach(fila => fila.style.animationPlayState = 'running');
+  
+    filas.forEach(fila => {
+      fila.addEventListener('touchstart', pauseAnimation);
+      fila.addEventListener('touchend', playAnimation);
+    });
+  
+    return () => {
+      filas.forEach(fila => {
+        fila.removeEventListener('touchstart', pauseAnimation);
+        fila.removeEventListener('touchend', playAnimation);
+      });
+    }
+  }, []);
+
+  // Duplicamos las tecnologías para scroll infinito
+  const filaSuperior = Array(5).fill(tecnologiasUno).flat();
+  const filaInferior = Array(5).fill(tecnologiasDos).flat();
   
   return (
-    // Tiutulo
-    <section  id='habilidades' className='container-fluid seccion-habilidades'>
+
+    <section  id='habilidades' className='seccion-habilidades container-fluid'>
+      {/* Titulo */}
       <div className='contenedor-titulo'>
         <h2 className='habilidades-titulo text-center'>Habilidades</h2>
       </div>
 
-      <div className="row contenedor-habilidades">
+      <div className='contenedor-habilidades'>
+        {/* Fila superior */}
+        <div className='fila fila-superior col-12'>
+          {[...filaSuperior, ...filaSuperior].map((tecno, index) => (
+            <div key={index} className='card'>
+              <img src={tecno.src} alt={tecno.alt} />
+            </div>
+          ))}
+        </div>
 
-        <div className='card card-html col-6 col-md-4 col-lg-3'>
-          <img src="public/imgs/tecnologias/html.png" alt="html"/>
+        {/* Fila inferior */}
+        <div className='fila fila-inferior col-12'>
+          {[...filaInferior, ...filaInferior].map((tecno, index) => (
+            <div key={index} className='card'>
+              <img src={tecno.src} alt={tecno.alt} />
+            </div>
+          ))}
         </div>
-        <div className='card card-html col-6 col-md-4  col-lg-3'>
-          <img src="public/imgs/tecnologias/css.png" alt="css"/>
-        </div>
-        <div className='card card-html col-6 col-md-4 col-lg-3'>
-          <img src="public/imgs/tecnologias/sass.png" alt="sass"/>
-        </div>
-        <div className='card card-html col-6 col-md-4 col-lg-3'>
-          <img src="public/imgs/tecnologias/bootstrap.png" alt=""/>
-        </div>
-        <div className='card card-html col-6 col-md-4 col-lg-3'>
-          <img src="public/imgs/tecnologias/js.png" alt=""/>
-        </div>
-        <div className='card card-html col-6 col-md-4 col-lg-3'>
-          <img src="public/imgs/tecnologias/react.png" alt=""/>
-        </div>
-        <div className='card card-html col-6 col-md-4 col-lg-3'>
-          <img src="public/imgs/tecnologias/nodejs.png" alt=""/>
-        </div>
-        <div className='card card-html col-6 col-md-4 col-lg-3'>
-          <img src="public/imgs/tecnologias/mongodb.png" alt=""/>
-        </div>
-    
+
       </div>
     </section>
   )
