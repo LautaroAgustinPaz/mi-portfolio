69d078e2be91d0b989b21c439ed6ffecc9d70c56
SamyazaCode <lautaropaz__@hotmail.com>
2025-06-12 18:11:55 -0300

    Nuevos cambios en sección Poryectos y limpieza de codigo

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Banner from './components/Banner'
 import Navbar from './components/Navbar'
 import SobreMi from './components/SobreMi'
 import Footer from './components/Footer'
+import Proyectos from './components/Proyectos'
 import Habilidades from './components/Habilidades'
 
 
@@ -13,6 +14,7 @@ function App() {
       <Banner></Banner>
       <SobreMi></SobreMi>
       <Habilidades></Habilidades>
+      <Proyectos></Proyectos>
       <Footer></Footer>
     </>
   )
---
+++ src/components/Proyectos.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const proyectos = [{
+  id: 1,
+  titulo: 'Portfolio Tatuador',
+  imagen: "imgs/proyectos/portfolio-tatuador.jpeg",
+  parrafo: "",
+  link: "https://landing-page-tatto-portfolio.vercel.app/",
+  repositorio: "https://github.com/LautaroAgustinPaz/landingPage-TattoPortfolio"
+},
+{
+  id: 2,
+  titulo: 'Portfolio Tatuador',
+  imagen: "imgs/proyectos/portfolio-tatuador.jpeg",
+  parrafo: "",
+  link: "https://landing-page-tatto-portfolio.vercel.app/",
+  repositorio: "https://github.com/LautaroAgustinPaz/landingPage-TattoPortfolio"
+},
+{
+  id: 3,
+  titulo: 'Portfolio Tatuador',
+  imagen: "imgs/proyectos/portfolio-tatuador.jpeg",
+  parrafo: "",
+  link: "https://landing-page-tatto-portfolio.vercel.app/",
+  repositorio: "https://github.com/LautaroAgustinPaz/landingPage-TattoPortfolio"
+},
+{
+  id: 4,
+  titulo: 'Portfolio Tatuador',
+  imagen: "imgs/proyectos/portfolio-tatuador.jpeg",
+  parrafo: "",
+  link: "https://landing-page-tatto-portfolio.vercel.app/",
+  repositorio: "https://github.com/LautaroAgustinPaz/landingPage-TattoPortfolio"
+},
+]
+
+const Proyectos = () => {
+
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <section id="proyectos" className='seccion-proyectos container-fluid'>
+    
+        {/* Titulo     */}
+        <div className='contenedor-titulo'>
+            <h2 className='habilidades-titulo text-center'>Proyectos</h2>
+        </div>
+
+        {/* Slide */}
+        <Slider {...settings}>
+          {proyectos.map((proyecto) => (
+            <div key={proyecto.id} className='slide-proyecto'>
+              <img src={proyecto.imagen} alt={proyecto.titulo} />
+              <h3>{proyecto.titulo}</h3>
+              <p>{proyecto.parrafo}</p>
+              <a href={proyecto.link} target='_blank'></a>
+              <a href={proyecto.repositorio} target='_blank'></a>
+            </div>
+          ))}
+        </Slider>
+
+    </section>
+  )
+}
+
+export default Proyectos
